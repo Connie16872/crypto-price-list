ae375ffb751d2850d2eaf8dcfa579573d7cb63f4
Connie16872 <connie16872@gmail.com>
2022-05-20 10:13:33 +0800

    reupload2

--- src/hooks/useCoin.js
+++ src/hooks/useCoin.js
@@ -7,7 +7,7 @@ export default function useCoin() {
   const fetchCoins = useCallback(async ({ page, per_page }) => {
     try {
       const res = await axios.get(
-        https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${per_page}&page=${page}&sparkline=true&price_change_percentage=7d
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${per_page}&page=${page}&sparkline=true&price_change_percentage=7d`
       )
       setCoins(res.data)
     } catch (error) {
