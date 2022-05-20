cdc4542f6af15618d06d70aca1e931c6d00b3010
Connie16872 <connie16872@gmail.com>
2022-05-20 10:07:03 +0800

    reupload

--- src/hooks/useCoin.js
+++ src/hooks/useCoin.js
@@ -1,19 +1,19 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 export default function useCoin() {
   const [coins, setCoins] = useState([])
 
-  const fetchCoins = async ({ page, per_page }) => {
+  const fetchCoins = useCallback(async ({ page, per_page }) => {
     try {
       const res = await axios.get(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${per_page}&page=${page}&sparkline=true&price_change_percentage=7d`
+        https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=${per_page}&page=${page}&sparkline=true&price_change_percentage=7d
       )
       setCoins(res.data)
     } catch (error) {
       return Promise.reject(error)
     }
-  }
+  }, [])
 
   return {
     coins,
